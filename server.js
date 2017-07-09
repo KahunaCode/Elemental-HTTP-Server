12a03d0665ee99fe05b781e8899c6a35656c4451
James Moore <jameslmoore@gmail.com>
2017-07-08 19:34:04 -1000

    object destructuring beeeooootcchhhhh

--- server.js
+++ server.js
@@ -2,49 +2,46 @@
 
 const http = require('http');
 const fs = require('fs');
+const qs = require('querystring');
 
 //let htmlPages = ['index.html', 'helium.html'];
 
-function parseWrite(body){
-  var elName = body.split('&')[0].split('=')[1];
-  var elSymb = body.split('&')[1].split('=')[1];
-  var elAtom = body.split('&')[2].split('=')[1];
-  var elDesc = body.split('&')[3].split('=')[1];
+function parseWrite({elementName, elementSymbol, elementAtomicNumber, elementDescription}){
 
   var page = `<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>The Elements - ${elName}</title>
+  <title>The Elements - ${elementName}</title>
   <link rel="stylesheet" href="/css/styles.css">
 </head>
 <body>
-  <h1>${elName}</h1>
-  <h2>${elSymb}</h2>
-  <h3>Atomic number ${elAtom}</h3>
-  <p>${elDesc}</p>
+  <h1>${elementName}</h1>
+  <h2>${elementSymbol}</h2>
+  <h3>Atomic number ${elementAtomicNumber}</h3>
+  <p>${elementDescription}</p>
   <p><a href="/">back</a></p>
   </body>
   </html>`;
 
-  fs.writeFile(`./public/${elName}.html`, page, (err) =>{
+  fs.writeFile(`./public/${elementName}.html`, page, (err) =>{
     if (err) throw err;
     console.log("file saved");
   });
 }
 
 
-
-
 const server = http.createServer(function(req, res) {
   console.log("client connected");
   const { method, url } = req;
   console.log(url);
-  let body = [];
+  let body = "";
   req.on('data', (chunk) => {
-    body.push(chunk);
+    //body.push(chunk);
+    body+=chunk;
   }).on('end', () => {
-    body=Buffer.concat(body).toString();
+    //body=Buffer.concat(body).toString();
+    body = qs.parse(body);
     console.log("body is:", body);
 
     if (req.url === "/element" && method === "POST") {
@@ -52,9 +49,9 @@ const server = http.createServer(function(req, res) {
       parseWrite(body);
     }
 
+
   });
 
-  //console.log(req);
 
   res.end();
 });
