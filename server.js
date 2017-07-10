4dcddb6c3a683eac4cf61b421c666880969ce0fb
James Moore <jameslmoore@gmail.com>
2017-07-09 21:20:26 -1000

    added http server url parsing and serving pages to client

--- server.js
+++ server.js
@@ -7,39 +7,50 @@ const qs = require('querystring');
 var htmlPages = fs.readdirSync('./public');
 console.log("files in /public", htmlPages);
 
-
+//parse POST and write html files
 function parseWrite({elementName, elementSymbol, elementAtomicNumber, elementDescription}){
-
   var page = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>The Elements - ${elementName}</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <h1>${elementName}</h1>
-  <h2>${elementSymbol}</h2>
-  <h3>Atomic number ${elementAtomicNumber}</h3>
-  <p>${elementDescription}</p>
-  <p><a href="/">back</a></p>
-  </body>
-  </html>`;
-
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <title>The Elements - ${elementName}</title>
+        <link rel="stylesheet" href="/css/styles.css">
+      </head>
+      <body>
+        <h1>${elementName}</h1>
+        <h2>${elementSymbol}</h2>
+        <h3>Atomic number ${elementAtomicNumber}</h3>
+        <p>${elementDescription}</p>
+        <p><a href="/">back</a></p>
+      </body>
+    </html>`;
   fs.writeFile(`./public/${elementName}.html`, page, (err) =>{
     if (err) throw err;
     console.log("file saved");
   });
 }
 
+//write pages back to client
+function servePage(response, page){
+  console.log('serve this page:', page);
+  var body = fs.readFileSync(`./public/${page}`).toString();
+  response.write(body);
+  response.end();
+}
 
+//main server
 const server = http.createServer(function(req, res) {
   console.log("client connected");
   var { method, url } = req;
   url = url.slice(1);//remove "/" for comparison to htmlPages
   console.log("url is ",url);
-  if (htmlPages.includes(url)){
-        console.log('serve this page:', url);
+  //serve pages
+  if (htmlPages.includes(url) && method === 'GET'){
+        servePage(res, url);
+      }
+      else{
+        res.statusCode = 404;
+        res.end();
       }
 
   let body = "";
@@ -55,13 +66,10 @@ const server = http.createServer(function(req, res) {
       console.log("POST to /element");
       parseWrite(body);
     }
-
-
-
   });
 
-
   res.end();
+
 });
 
 server.listen(8080, function(){
